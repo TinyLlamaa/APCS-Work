b3dc83e3aa2593d5c0f6e1f8e3bb936d7695b9e0
TinyLlamaa <aleneabdulian26@marlborough.org>
2022-09-19 00:14:40 -0700

    i did something suuuuuper cool!

---
+++ unit1/Geometric/Geometric.java
@@ -0,0 +1,14 @@
+public class Geometric{
+
+  public static void main(String[] args){
+
+    int n = 3;
+    int c = 0;
+
+    for(int i = 0; i < 10; i++){
+      c = n;
+      n*=2;
+      System.out.println(c);
+    }
+  }
+}
